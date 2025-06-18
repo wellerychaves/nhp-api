cc4452608d850fdffae1332fa5a6350ed4d5548d
Wellery Chaves <wellerychaves@outlook.com>
2025-06-18 14:50:45 -0300

    fix: add auto update field

--- src/database/helpers/timestamps.helpers.ts
+++ src/database/helpers/timestamps.helpers.ts
@@ -2,11 +2,14 @@ import { timestamp } from "drizzle-orm/pg-core";
 import { z } from "zod";
 
 export const timestampsDrizzle = {
-	updatedAt: timestamp().defaultNow().notNull(),
-	createdAt: timestamp().defaultNow().notNull(),
+	createdAt: timestamp({ mode: "date" }).defaultNow().notNull(),
+	updatedAt: timestamp({ mode: "date" })
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 };
 
 export const timestampsZod = {
-	createdAt: z.date().default(new Date()),
 	updatedAt: z.date().default(new Date()),
+	createdAt: z.date().default(new Date()),
 };
