33d51092bd91ad609ae1d217c698a21957504d22
Wellery Chaves <wellerychaves@outlook.com>
2025-06-19 17:07:11 -0300

    feat(schema): add more fields to teams database

--- src/database/schemas/teams.schema.ts
+++ src/database/schemas/teams.schema.ts
@@ -1,9 +1,13 @@
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
 import { timestampsDrizzle } from "../helpers/timestamps.helpers";
 
 export const teamsTable = pgTable("teams", {
 	id: uuid().primaryKey(),
 	teamName: varchar().notNull(),
 	color: varchar().notNull(),
+	imageUrl: text(),
+	points: integer().default(0),
+	wins: integer().default(0),
+	defeats: integer().default(0),
 	...timestampsDrizzle,
 });
