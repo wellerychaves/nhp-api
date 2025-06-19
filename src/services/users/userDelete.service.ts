2ca0f1fc382490418ca656c61dd294b713477085
Wellery Chaves <wellerychaves@outlook.com>
2025-06-19 17:00:47 -0300

    style: fix captalization and change prop name to make clearer what it's about

--- src/services/users/userDelete.service.ts
+++ src/services/users/userDelete.service.ts
@@ -2,11 +2,11 @@ import { eq } from "drizzle-orm";
 import { db } from "../../database";
 import { usersTable } from "../../database/schemas/users.schema";
 
-export const deleteUserService = async (id: string) => {
-	const user = await db.delete(usersTable).where(eq(usersTable.id, id)).returning();
+export const deleteUserService = async (userId: string) => {
+	const user = await db.delete(usersTable).where(eq(usersTable.id, userId)).returning();
 
 	if (user.length === 0) {
-		throw new Error("User Not found.");
+		throw new Error("User not found.");
 	}
 
 	return user;
