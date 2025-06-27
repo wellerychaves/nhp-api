311318c8a5daf529f4e14cfd4741774beb24ac2a
Wellery Chaves <wellerychaves@outlook.com>
2025-06-27 13:03:00 -0300

    feat: update the query to order descending by points

--- src/services/teams/teamList.service.ts
+++ src/services/teams/teamList.service.ts
@@ -1,8 +1,9 @@
+import { desc } from "drizzle-orm";
 import { db } from "../../database";
 import { teamsTable } from "../../database/schemas/teams.schema";
 
 export const listTeamService = async () => {
-	const query = await db.select().from(teamsTable);
+	const query = await db.select().from(teamsTable).orderBy(desc(teamsTable.points));
 
 	const teams = {
 		total: query.length,
