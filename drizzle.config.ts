a5e1086dfbcfb4db3d69eb56faa426d2c89541f0
Wellery Chaves <wellerychaves@outlook.com>
2025-07-06 17:07:24 -0300

    feat: get database url from the env file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "drizzle-kit";
 
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+	throw new Error("DATABASE_URL não está definida nas variáveis de ambiente.");
+}
+
 export default defineConfig({
 	out: "./drizzle",
 	dialect: "postgresql",
 	schema: "./src/database/schemas",
 	dbCredentials: {
-		url: process.env.DATABASE_URL,
+		url: databaseUrl,
 	},
 });
